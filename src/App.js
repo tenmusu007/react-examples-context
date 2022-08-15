f329405c7e82f0a0381c6390de206323a260d457
adlascio <arthur.dilascio@gmail.com>
2022-08-15 00:26:43 -0700

    w05d1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Reducer from './Components/Reducer/Reducer';
 // import Class from './Components/Class/Class';
 // import Buttons from './Components/Buttons/Buttons';
 // import FifaTourney from './Components/FifaTourney/FifaTourney';
 // import Context from './Components/Context/Context';
 // import CartExercise from './Components/CartExercise/CartExercise';
 // import CartClassExercise from './Components/CartClassExercise/CartClassExercise';
-import Storybook from './Components/Storybook/Storybook';
+// import Storybook from './Components/Storybook/Storybook';
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
       {/* <FifaTourney /> */}
       {/* <Class /> */}
       {/* <CartClassExercise /> */}
-      <Storybook />
+      {/* <Storybook /> */}
+      <Reducer />
     </div>
   );
 }
---
+++ src/Components/Reducer/Reducer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Reducer() {
+  return (
+    <div className='component' style={{ display: 'flex' }}>
+      <h1>Reducer</h1>
+    </div>
+  );
+}
